d34785cc7d2da66a7db5d45dafbf7acec17b1a6a
Amesezra <43195970+Amesezra@users.noreply.github.com>
2022-10-11 11:26:49 -0400

    parsed data into daily weather output method. learned more about load() vs loads() and I/O behaviors when opening files.

--- venv/open_meteo.py
+++ venv/open_meteo.py
@@ -1,73 +1,97 @@
+#  https://bobbyhadz.com/blog/python-typeerror-the-json-object-must-be-str-bytes-or-bytearray-not-textiowrapper
+#  super helpful for figuring out load() vs loads()
+#  https://stackoverflow.com/questions/6648493/how-to-open-a-file-for-both-reading-and-writing
+#  I/O behaviors
 import datetime as dt
 import requests
 import json
 
-url = "https://api.open-meteo.com/v1/forecast?latitude=41.4993&longitude=-81.6944" \
-      "&hourly=temperature_2m,apparent_temperature,precipitation&daily=weathercode," \
-      "sunrise,sunset&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit" \
-      "=inch&timezone=America%2FNew_York"
+url = "https://api.open-meteo.com/v1/forecast?latitude=41.4993&longitude=-81.6944&hourly=temperature_2m," \
+      "apparent_temperature,precipitation&daily=weathercode,temperature_2m_max,temperature_2m_min," \
+      "apparent_temperature_max,apparent_temperature_min,sunrise,sunset&current_weather=true&temperature_unit" \
+      "=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York"
 
 response = requests.get(url).json()
 
-with open('weather_data.json', 'w') as f:
+with open('weather_data.json', 'w', encoding='utf-8') as f:
     json.dump(response, f, indent=3)
 
+with open('weather_data.json', 'r', encoding='utf-8') as f:
+    my_data = json.loads(f.read())
+
+print("Printing weather code details for the next seven days:")
+print(my_data["daily"]["weathercode"])
+print()
+
+
 while True:  # WMO weather interpretation codes (WW)
-    weather_code = response["daily"]["weathercode"][0]
+    weather_code = my_data["daily"]["weathercode"][0]
     if weather_code == 0:
-        print("Clear sky")
+        weather_code = "Clear sky"
     if weather_code == 1:
-        print('Mainly clear')
+        weather_code = 'Mainly clear'
     elif weather_code == 2:
-        print("partly cloudy")
+        weather_code = "partly cloudy"
     elif weather_code == 3:
-        print("overcast")
+        weather_code = "overcast"
     elif weather_code == 45:
-        print("Fog")
+        weather_code = "Fog"
     elif weather_code == 48:
-        print("Depositing rime fog")
+        weather_code = "Depositing rime fog"
     elif weather_code == 51:
-        print("Light drizzle")
+        weather_code = "Light drizzle"
     elif weather_code == 53:
-        print("Moderate drizzle")
+        weather_code = "Moderate drizzle"
     elif weather_code == 55:
-        print("Dense drizzle")
+        weather_code = "Dense drizzle"
     elif weather_code == 56:
-        print("Light freezing drizzle")
+        weather_code = "Light freezing drizzle"
     elif weather_code == 57:
-        print("Heavy freezing drizzle")
+        weather_code = "Heavy freezing drizzle"
     elif weather_code == 61:
-        print("Slight rain")
+        weather_code = "Slight rain"
     elif weather_code == 63:
-        print("Moderate rain")
+        weather_code = "Moderate rain"
     elif weather_code == 65:
-        print("Heavy rain")
+        weather_code = "Heavy rain"
     elif weather_code == 66:
-        print("Light freezing rain")
+        weather_code = "Light freezing rain"
     elif weather_code == 67:
-        print("Moderate freezing rain")
+        weather_code = "Moderate freezing rain"
     elif weather_code == 71:
-        print("Slight snowfall")
+        weather_code = "Slight snowfall"
     elif weather_code == 73:
-        print("Moderate snowfall")
+        weather_code = "Moderate snowfall"
     elif weather_code == 75:
-        print("Heavy snowfall")
+        weather_code = "Heavy snowfall"
     elif weather_code == 77:
-        print("Snow grains")
+        weather_code = "Snow grains"
     elif weather_code == 80:
-        print("Slight rain showers")
+        weather_code = "Slight rain showers"
     elif weather_code == 81:
-        print("Moderate rain showers")
+        weather_code = "Moderate rain showers"
     elif weather_code == 82:
-        print("Violent rain showers")
+        weather_code = "Violent rain showers"
     elif weather_code == 85:
-        print("Heavy snow showers")
+        weather_code = "Heavy snow showers"
     elif weather_code == 86:
-        print("Slight Thunderstorm")
+        weather_code = "Slight Thunderstorm"
     elif weather_code == 95:
-        print("Moderate Thunderstorm")
+        weather_code = "Moderate Thunderstorm"
     elif weather_code == 96:
-        print("Thunderstorm with slight hail")
+        weather_code = "Thunderstorm with slight hail"
     elif weather_code == 99:
-        print("Thunderstorm with slight and heavy hail")
+        weather_code = "Thunderstorm with slight and heavy hail"
     break
+
+print(f"The current tempurature is:", my_data["current_weather"]["temperature"],
+      my_data["daily_units"]["temperature_2m_max"], "\nToday you can expect",
+      weather_code, "weather,""\nwith a windspead of:", my_data["current_weather"]["windspeed"],
+      "mph in the direction of:", my_data["current_weather"]["winddirection"])
+
+print(f"You can expect a high of", my_data["daily"]["temperature_2m_max"][0],
+      my_data["daily_units"]["temperature_2m_max"], "and a low of", my_data["daily"]["temperature_2m_min"][0],
+      my_data["daily_units"]["temperature_2m_max"])
+print(f"The real feel today will be between", my_data["daily"]["apparent_temperature_max"][0],
+      my_data["daily_units"]["temperature_2m_max"], "-", my_data["daily"]["apparent_temperature_min"][0],
+      my_data["daily_units"]["temperature_2m_max"])
